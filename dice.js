f9695f5e592ea2e34262b8521d83fd57a727df1e
BartekPpr <bartoszpolak.pr@gmail.com>
2025-07-22 21:56:39 +0200

    Delete dice.js

--- dice.js
+++
@@ -1,9 +0,0 @@
-let allDice = ['1.jpg',  '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
-let outcome = document.querySelector('#outcome');
-
-function Roll(){
-    let draw = Math.floor(Math.random() * allDice.length);
-    outcome.src = allDice[draw];
-}
-    
-   
